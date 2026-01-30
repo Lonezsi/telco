0be54f6a39ea594aedbc419b1ef6bf0da09b2e5c
ak4ext <ak4ext@inf.elte.hu>
2026-01-30 22:00:53 +0100

    minor changes

--- incompatible_feeds/telco-integration/backend/src/main/java/com/telco/integration/web/ProductController.java
+++ incompatible_feeds/telco-integration/backend/src/main/java/com/telco/integration/web/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/products") // might need to be /api/products in real deployment
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class ProductController {
